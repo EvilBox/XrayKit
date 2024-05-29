d3fcb9060e6c6b0f97183643cd9fb4e200976ceb
Roman Vasilev <roman@MacBook-Pro-Roman.local>
2024-05-30 01:04:54 +0400

    tst

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/romaxa55/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
-      checksum: "457f16ce5b68f544959fb54e5490d1347a92232ab7cae0f406f9bc992072e551"
+      url: "https://github.com/romaxa55/XrayKit/releases/download/0.1.61/XrayKit.xcframework.zip",
+      checksum: "c27a231e665856afa9fc9baf229edc1478c7902ae4fa8e76910deee715d4de0d"
     )
   ]
 )
